c6b870d91a9e91fb719d76e4090e0c2e2e12f3c0
Rahul Sharma <rahul.fabled@gmail.com>
2020-08-05 13:25:49 +0545

    Correct class name of description

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -5,7 +5,7 @@ function Project(props) {
   return (
     <div className="project">
       <h1 className="project__heading"><a className="project__link" href={props.project.url} title={props.project.name} target="_blank" rel="noopener noreferrer">{props.project.name}</a></h1>
-      <div className="description">
+      <div className="project__description">
         {props.project.description? props.project.description: DEFAULT_DESCRIPTION}
       </div>
     </div>
